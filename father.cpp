f672685e644ec6344ed10e85ec60befd4fbb7562
Sakib Al Hasan <76560286+saifsaki@users.noreply.github.com>
2021-07-09 14:14:00 +0600

    Add files via upload

---
+++ father.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+	int x, y, result, fathers_age; // x = boy's age and y = father's age;
+	y = 3 * x;
+	result = (36-4) / 2; 
+	fathers_age = result * 3;
+	cout << "Father's age is : " << " " << fathers_age << " and "  << "Son's age is " << result;
+	return 0;
+}
---
+++ transport.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int a,b;
+	cin >> a;
+	b =  50-a%50;
+	cout << b;
+	return 0;
+}
